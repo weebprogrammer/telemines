1e8fc870097f7f32cfb50202c567fe54f5d44bd8
Puck Inside <saginov606@gmail.com>
2023-08-27 05:25:23 +0500

    Added a temporary interaction interface.

--- Program.cs
+++ Program.cs
@@ -24,9 +24,43 @@ class Program
 
         Console.WriteLine(message.Chat.FirstName + "\t || \t" + message.Text);
 
-        if (message.Text.ToLower().Contains("/gamestart"))
+        if (message.Text.ToLower().Contains("/start"))
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Для ознакомления напишите /help");
+            return;
+        }
+        else if (message.Text.ToLower().Contains("/gamestart"))
         {
             await client.SendTextMessageAsync(message.Chat.Id, "Игра началась!");
+            return;
+        }
+        else if (message.Text.ToLower().Contains("/gamestop"))
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Игра закончилась!");
+            return;
+        }
+        else if (message.Text.ToLower().Contains("/choice"))
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Выбрана клетка ...");
+            return;
+        }
+        else if (message.Text.ToLower().Contains("/mark"))
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Выбрана марка ...");
+            return;
+        }
+        else if (message.Text.ToLower().Contains("/help"))
+        {
+            string helpMessage = "";
+
+            helpMessage += "/gamestart - начать игру\n";
+            helpMessage += "/gamestop - завершить игру\n";
+            helpMessage += "/choice column <value> - выборать столбец\n";
+            helpMessage += "/choice row <value> - выборать строку\n";
+            helpMessage += "/mark column <value> - выборать столбец\n";
+            helpMessage += "/mark row <value> - выборать строку\n";
+
+            await client.SendTextMessageAsync(message.Chat.Id, helpMessage);
             return;
         }
 
