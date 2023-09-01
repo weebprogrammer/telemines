fc700e4e7bfc75fd37e8b9237e62e6fa97755204
amirtagashov <amirtagashov@gmail.com>
2023-09-01 13:37:40 +0500

    board testing

--- Minesweeper/Cell.cs
+++ Minesweeper/Cell.cs
@@ -1,12 +1,12 @@
-namespace telemines.Minesweper;
+namespace telemines.Minesweeper;
 
 
-enum CellType {
+public enum CellType {
     Mine, 
     Num,
 }
 
-struct Cell
+public struct Cell
 {
     public CellType type;
     public int? value; 
--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
 ﻿namespace telemines;
 
+using telemines.Minesweeper;
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try {
+            var board = new Board(4,4);
+            board.ForEachCell((cell) => {
+                cell.type = CellType.Num;
+                cell.value = 5;
+                return cell;
+            });
+
+            board.ForEachCell((cell) => {
+                
+            });
+        } catch(Exception e) {
+            Console.WriteLine(e.ToString());
+        }
     }
 }
