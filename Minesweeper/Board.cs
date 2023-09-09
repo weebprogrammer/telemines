98e33da0726010901818248db0157a7b0fe89b51
PuckInside <140918458+PuckInside@users.noreply.github.com>
2023-09-09 20:51:35 +0500

    Fix Indecs Out Of Range
    Фуфельшмерц пакостин корпорейтет

--- Minesweeper/Board.cs
+++ Minesweeper/Board.cs
@@ -18,13 +18,13 @@ public class Board
 
 	public Cell GetCellPosition(int x, int y)
 	{
-		return cells[(x * y)-1];
+		return cells[(x * Width)+y];
 	}
 
 	public void SetCellPosition(int x, int y, CellType type, int? value = null)
 	{
-		cells[(x * y)-1].type = type;
-		cells[(x * y)-1].value = value;
+		cells[x * Width)+y].type = type;
+		cells[x * Width)+y].value = value;
 	}
 
 	public void ForEachCell(Func<Cell, Cell> Action)
